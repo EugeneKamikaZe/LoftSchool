169f804d1be08d0ec3192272453abf052d248617
Eugene <egor_egorov_qwerty@mail.ru>
2019-06-10 10:40:37 +0300

    5 with *

--- src/dnd.js
+++ src/dnd.js
@@ -2,14 +2,18 @@
 
 /*
  Создайте страницу с кнопкой.
- При нажатии на кнопку должен создаваться div со случайными размерами, цветом и позицией на экране
- Необходимо предоставить возможность перетаскивать созданные div при помощи drag and drop
- Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ При нажатии на кнопку должен создаваться div со случайными размерами,
+ цветом и позицией на экране
+ Необходимо предоставить возможность перетаскивать созданные div при помощи
+ drag and drop
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем,
+ что встроено в браузер
  */
 
 /*
  homeworkContainer - это контейнер для всех ваших домашних заданий
- Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+ Если вы создаете новые html-элементы и добавляете их на страницу, то
+ дабавляйте их только в этот контейнер
 
  Пример:
    const newDiv = document.createElement('div');
@@ -18,19 +22,46 @@
 const homeworkContainer = document.querySelector('#homework-container');
 
 /*
- Функция должна создавать и возвращать новый div с классом draggable-div и случайными размерами/цветом/позицией
+ Функция должна создавать и возвращать новый div с классом draggable-div и
+ случайными размерами/цветом/позицией
  Функция должна только создавать элемент и задвать ему случайные размер/позицию/цвет
- Функция НЕ должна добавлять элемент на страницу. На страницу элемент добавляется отдельно
+ Функция НЕ должна добавлять элемент на страницу. На страницу элемент добавляется
+ отдельно
 
  Пример:
    const newDiv = createDiv();
    homeworkContainer.appendChild(newDiv);
  */
 function createDiv() {
+    let  button  =  document.createElement('button'),
+        div = document.createElement('div');
+
+    document.homeworkContainer.appendChild (button);
+    button.innerText = 'Создать рандомный Div';
+
+    function getRandomColor() {
+        let letters = '0123456789ABCDEF',
+            color = '#';
+
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    button.addEventListener('click', () => {
+        div.style.width = (Math.random() * 100) + '%';
+        div.style.height = (Math.random() * 100) + '%';
+        div.style.backgroundColor = getRandomColor();
+        div.style.top = (Math.random() * 100) + '%';
+        div.style.left = (Math.random() * 100) + '%';
+        document.homeworkContainer.appendChild(div);
+    });
 }
 
 /*
- Функция должна добавлять обработчики событий для перетаскивания элемента при помощи drag and drop
+ Функция должна добавлять обработчики событий для перетаскивания элемента при
+ помощи drag and drop
 
  Пример:
    const newDiv = createDiv();
@@ -50,8 +81,10 @@ addDivButton.addEventListener('click', function() {
     homeworkContainer.appendChild(div);
     // назначить обработчики событий мыши для реализации D&D
     addListeners(div);
-    // можно не назначать обработчики событий каждому div в отдельности, а использовать делегирование
-    // или использовать HTML5 D&D - https://www.html5rocks.com/ru/tutorials/dnd/basics/
+    // можно не назначать обработчики событий каждому div в отдельности, а
+    // использовать делегирование
+    // или использовать HTML5 D&D -
+    // https://www.html5rocks.com/ru/tutorials/dnd/basics/
 });
 
 export {
