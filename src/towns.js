5bd9a82f05292bb8f7eea75959877670b3b9a811
Eugene <egor_egorov_qwerty@mail.ru>
2019-06-11 20:46:07 +0300

    json

--- src/towns.js
+++ src/towns.js
@@ -5,24 +5,28 @@
 
  При вводе в текстовое поле, под ним должен появляться список тех городов,
  в названии которых, хотя бы частично, есть введенное значение.
- Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
+ Регистр символов учитываться не должен, то есть "Moscow" и "moscow" -
+ одинаковые названия.
 
  Во время загрузки городов, на странице должна быть надпись "Загрузка..."
  После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
 
  Разметку смотрите в файле towns-content.hbs
 
- Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только
+ тем, что встроено в браузер
 
  *** Часть со звездочкой ***
- Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
+ Если загрузка городов не удалась (например, отключился интернет или
+ сервер вернул ошибку),
  то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
  При клике на кнопку, процесс загрузки повторяется заново
  */
 
 /*
  homeworkContainer - это контейнер для всех ваших домашних заданий
- Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
+ Если вы создаете новые html-элементы и добавляете их на страницу, то
+ добавляйте их только в этот контейнер
 
  Пример:
    const newDiv = document.createElement('div');
@@ -31,12 +35,35 @@
 const homeworkContainer = document.querySelector('#homework-container');
 
 /*
- Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+ Функция должна вернуть Promise, который должен быть разрешен с массивом
+ городов в качестве значения
 
  Массив городов пожно получить отправив асинхронный запрос по адресу
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
  */
 function loadTowns() {
+    return towns = new Promise(function (resolve) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json', true);
+        xhr.responseType = 'json';
+        xhr.addEventListener('load', () => {
+            loadingBlock.style.display = 'none';
+            filterBlock.style.display = 'block';
+
+            resolve(xhr.response.sort(function (a, b) {
+                if (a.name < b.name) {
+                    return -1;
+                }
+                if (a.name > b.name) {
+                    return 1;
+                }
+
+                return 0;
+            }));
+        });
+
+        xhr.send();
+    });
 }
 
 /*
@@ -51,7 +78,15 @@ function loadTowns() {
    isMatching('Moscow', 'Moscov') // false
  */
 function isMatching(full, chunk) {
+    if (full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+let towns,
+    cities = [];
 
 /* Блок с надписью "Загрузка" */
 const loadingBlock = homeworkContainer.querySelector('#loading-block');
@@ -62,8 +97,21 @@ const filterInput = homeworkContainer.querySelector('#filter-input');
 /* Блок с результатами поиска */
 const filterResult = homeworkContainer.querySelector('#filter-result');
 
+
+loadTowns().then(res => {
+                cities = res;
+                loadingBlock.style.display = 'none';
+                filterBlock.style.display = 'block';
+            });
+
 filterInput.addEventListener('keyup', function() {
     // это обработчик нажатия кливиш в текстовом поле
+    filterResult.innerHTML = filterInput.value ?
+        cities
+            .filter(item => isMatching(item.name, filterInput.value))
+            .map(item => `${item.name}`)
+            .join('')
+        : '';
 });
 
 export {
